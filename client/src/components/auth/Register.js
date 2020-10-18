b87b6d48e5129e4090b54fdd95b4a22fb96d7096
Harris Fauntleroy <harrisfauntleroy@gmail.com>
2020-10-19 09:40:37 +1000

    login and register form, useState react hook

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,16 +1,53 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
+    });
+
+    const { name, email, password, password2 } = formData;
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        if (password !== password2) {
+            console.log('Passwords do not match');
+        } else {
+            console.log('SUCCESS');
+        }
+    };
+
     return (
         <Fragment>
             <h1 className="large text-primary">Sign Up</h1>
-            <p className="lead"><i class="fas fa-user"></i> Create Your Account</p>
-            <form className="form" action="create-profile.html">
+            <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+            <form className="form" onSubmit={e => onSubmit(e)}>
                 <div className="form-group">
-                    <input type="text" placeholder="Name" name="name" required />
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        autoComplete="username"
+                        value={name}
+                        onChange={e => onChange(e)}
+                        required
+                    />
                 </div>
                 <div className="form-group">
-                    <input type="email" placeholder="Email Address" name="email" />
+                    <input
+                        type="email"
+                        placeholder="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        value={email}
+                        onChange={e => onChange(e)}
+                        required
+                    />
                     <small className="form-text"
                     >This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small
@@ -20,22 +57,34 @@ export const Register = () => {
                     <input
                         type="password"
                         placeholder="Password"
+                        autoComplete="new-password"
                         name="password"
                         minLength="6"
+                        value={password}
+                        onChange={e => onChange(e)}
+                        required
                     />
                 </div>
                 <div className="form-group">
                     <input
                         type="password"
                         placeholder="Confirm Password"
+                        autoComplete="new-password"
                         name="password2"
                         minLength="6"
+                        value={password2}
+                        onChange={e => onChange(e)}
+                        required
                     />
                 </div>
-                <input type="submit" className="btn btn-primary" value="Register" />
+                <input
+                    type="submit"
+                    className="btn btn-primary"
+                    value="Register"
+                />
             </form>
             <p className="my-1">
-                Already have an account? <a href="login.html">Sign In</a>
+                Already have an account? <a href="/login">Sign In</a>
             </p>
         </Fragment>
     )
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,10 +6,9 @@ export const Landing = () => {
         <section className="landing">
             <div className="dark-overlay">
                 <div className="landing-inner">
-                    <h1 className="x-large">Developer Connector</h1>
+                    <h1 className="x-large">Linked list</h1>
                     <p className="lead">
-                        Create a developer profile/portfolio, share posts and get help from
-                        other developers
+                        Social networking for developers from front to back.
             </p>
                     <div className="buttons">
                         <Link to="/register" className="btn btn-primary">Sign Up</Link>
