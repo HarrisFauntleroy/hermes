5d98c701f1f6b2fe6bb8ef2be2ca9c24e0df5c27
Harris Fauntleroy <harrisfauntleroy@gmail.com>
2020-10-21 06:05:57 +1000

    work on alerts

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useState } from 'react'
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
 
-export const Register = () => {
+export const Register = (props) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -16,7 +18,7 @@ export const Register = () => {
     const onSubmit = async e => {
         e.preventDefault();
         if (password !== password2) {
-            console.log('Passwords do not match');
+            props.setAlert('Passwords do not match', 'danger');
         } else {
             console.log('SUCCESS');
         }
